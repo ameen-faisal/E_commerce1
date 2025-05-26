02a54d0e356ae3421573e2ee67714ef79eda63e3
eng Ameen faisal <engameen07@gmail.com>
2025-05-26 23:43:01 +0300

    log in signup form

---
+++ E-commerce/Presentation_Layer/loginSignUpForm.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace E_commerce.Presentation_Layer
+{
+    partial class loginSignUpForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loginSignUpForm));
+            this.button13 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.signUp1 = new E_commerce.Presentation_Layer.signUp();
+            this.login1 = new E_commerce.Presentation_Layer.login();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button13
+            // 
+            this.button13.FlatAppearance.BorderSize = 0;
+            this.button13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button13.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button13.ForeColor = System.Drawing.Color.White;
+            this.button13.Image = ((System.Drawing.Image)(resources.GetObject("button13.Image")));
+            this.button13.Location = new System.Drawing.Point(881, 2);
+            this.button13.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(27, 43);
+            this.button13.TabIndex = 6;
+            this.button13.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::E_commerce.Properties.Resources.colorLOGIN;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Location = new System.Drawing.Point(-24, -37);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(517, 582);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.BlueViolet;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(224, 492);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 42);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "SignUp";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseLeave += new System.EventHandler(this.button2_MouseLeave);
+            this.button2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button2_MouseMove);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.BlueViolet;
+            this.label3.Font = new System.Drawing.Font("MV Boli", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(174, 489);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 45);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "or";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.BlueViolet;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(111, 492);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 42);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Login";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.BlueViolet;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(133, 330);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(204, 45);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Application";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.BlueViolet;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(38, 292);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(479, 40);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Welcome To The E-commerce ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.BlueViolet;
+            this.panel4.BackgroundImage = global::E_commerce.Properties.Resources.pngwing_com;
+            this.panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel4.Location = new System.Drawing.Point(72, 60);
+            this.panel4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(336, 290);
+            this.panel4.TabIndex = 6;
+            // 
+            // signUp1
+            // 
+            this.signUp1.BackColor = System.Drawing.Color.White;
+            this.signUp1.Location = new System.Drawing.Point(518, 15);
+            this.signUp1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.signUp1.Name = "signUp1";
+            this.signUp1.Size = new System.Drawing.Size(359, 482);
+            this.signUp1.TabIndex = 8;
+            this.signUp1.Load += new System.EventHandler(this.SignUp1_Load);
+            // 
+            // login1
+            // 
+            this.login1.BackColor = System.Drawing.Color.White;
+            this.login1.Location = new System.Drawing.Point(518, 15);
+            this.login1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.login1.Name = "login1";
+            this.login1.Size = new System.Drawing.Size(359, 482);
+            this.login1.TabIndex = 7;
+            // 
+            // loginSignUpForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(915, 506);
+            this.Controls.Add(this.signUp1);
+            this.Controls.Add(this.login1);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "loginSignUpForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "loginSignUpForm";
+            this.Load += new System.EventHandler(this.loginSignUpForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private login login1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private signUp signUp1;
+    }
+}
---
+++ E-commerce/Presentation_Layer/loginSignUpForm.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace E_commerce.Presentation_Layer
+{
+    public partial class loginSignUpForm : Form
+    {
+        public loginSignUpForm()
+        {
+            InitializeComponent();
+            login1.BringToFront();
+
+
+
+           /* string mysqlCon = "server=localhost; user=root; database=e-commerce; password=";
+            MySqlConnection mySqlConnection = new MySqlConnection(mysqlCon);
+            try
+            {
+                mySqlConnection.Open();
+                MessageBox.Show("connection success");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                mySqlConnection.Close();
+            }
+            */
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_MouseMove(object sender, MouseEventArgs e)
+        {
+            button2.Font = new Font(button2.Font, FontStyle.Underline);
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+            button2.Font = new Font(button2.Font, FontStyle.Regular);
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            button1.Font = new Font(button1.Font, FontStyle.Underline);
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.Font = new Font(button1.Font, FontStyle.Regular);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            signUp1.BringToFront();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            login1.BringToFront();
+        }
+
+        private void loginSignUpForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SignUp1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
