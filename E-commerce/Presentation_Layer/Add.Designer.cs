faf73f2936648c76d878e488641aa083d3fce358
eng Ameen faisal <engameen07@gmail.com>
2025-05-26 23:45:12 +0300

    Add files via upload

---
+++ E-commerce/Presentation_Layer/Add.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace E_commerce.Presentation_Layer
+{
+    partial class Add
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.productName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.productNameBox = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.productPricBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Forte", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(8, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add a new product:";
+            // 
+            // productName
+            // 
+            this.productName.AutoSize = true;
+            this.productName.Font = new System.Drawing.Font("Times New Roman", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.productName.Location = new System.Drawing.Point(153, 177);
+            this.productName.Name = "productName";
+            this.productName.Size = new System.Drawing.Size(176, 33);
+            this.productName.TabIndex = 1;
+            this.productName.Text = "product name:";
+            this.productName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(152, 257);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 33);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "product image:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(152, 330);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 33);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "product price:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = global::E_commerce.Properties.Resources.inputColor;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Controls.Add(this.productNameBox);
+            this.panel2.Location = new System.Drawing.Point(330, 166);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(429, 60);
+            this.panel2.TabIndex = 10;
+            // 
+            // productNameBox
+            // 
+            this.productNameBox.BackColor = System.Drawing.Color.Gainsboro;
+            this.productNameBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.productNameBox.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.productNameBox.ForeColor = System.Drawing.Color.DimGray;
+            this.productNameBox.Location = new System.Drawing.Point(30, 20);
+            this.productNameBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.productNameBox.Name = "productNameBox";
+            this.productNameBox.Size = new System.Drawing.Size(367, 17);
+            this.productNameBox.TabIndex = 9;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::E_commerce.Properties.Resources.inputColor;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.productPricBox);
+            this.panel1.Location = new System.Drawing.Point(330, 315);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(429, 60);
+            this.panel1.TabIndex = 11;
+            // 
+            // productPricBox
+            // 
+            this.productPricBox.BackColor = System.Drawing.Color.Gainsboro;
+            this.productPricBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.productPricBox.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.productPricBox.ForeColor = System.Drawing.Color.DimGray;
+            this.productPricBox.Location = new System.Drawing.Point(30, 20);
+            this.productPricBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.productPricBox.Name = "productPricBox";
+            this.productPricBox.Size = new System.Drawing.Size(367, 17);
+            this.productPricBox.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.BlueViolet;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 19F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.button1.ForeColor = System.Drawing.Color.BlueViolet;
+            this.button1.Location = new System.Drawing.Point(185, 453);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 53);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Resite";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.BlueViolet;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Tahoma", 19F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(464, 452);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(264, 54);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Submit";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.button2.Location = new System.Drawing.Point(360, 255);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 34);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Click";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // Add
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.productName);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Add";
+            this.Size = new System.Drawing.Size(902, 555);
+            this.Load += new System.EventHandler(this.Add_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label productName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox productNameBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox productPricBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+    }
+}
---
+++ E-commerce/Presentation_Layer/Add.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace E_commerce.Presentation_Layer
+{
+    public partial class Add : UserControl
+    {
+        string imageName;
+        string imagePath;
+        string targetPath;
+        public Add()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk_1(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string projrctPath = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
+            projrctPath = Directory.GetParent(projrctPath).FullName;
+            MessageBox.Show(projrctPath.Replace("\\", "\\\\"));
+
+            openFileDialog1.AddExtension = true;
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = false;
+            openFileDialog1.Filter = "Image File (*.JPG; *.PNG)|*.JPG; *.PNG";
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                imagePath = openFileDialog1.FileName;
+                 imageName = openFileDialog1.SafeFileName;
+                MessageBox.Show(imageName);
+
+                targetPath = Path.Combine("D:\\Muneer\\E-commerce\\E-commerce\\Presentation_Layer\\photo", imageName);
+                    
+                
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string name = productNameBox.Text;
+            string price = productPricBox.Text;
+            File.Copy(imagePath, targetPath, true);
+            Business_Layer.Product product= new Business_Layer.Product();
+            product.insertProduct(name, price, imageName);
+        }
+
+        private void Add_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
