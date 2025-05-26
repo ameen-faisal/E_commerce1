5def642268991276924eff9d1b7c2bd56887c223
AymanAbdelkaderMohammed <ab433139@gmail.com>
2025-05-26 23:34:00 +0300

    Delete Form
    Delete Form as Designer an code

---
+++ E-commerce/Presentation_Layer/delete.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace E_commerce.Presentation_Layer
+{
+    partial class delete
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Forte", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(3, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Delete products:";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 62);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(773, 389);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // delete
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.Name = "delete";
+            this.Size = new System.Drawing.Size(773, 451);
+            this.Load += new System.EventHandler(this.modify_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
---
+++ E-commerce/Presentation_Layer/delete.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+namespace E_commerce.Presentation_Layer
+{
+    public partial class delete : UserControl
+    {
+        public delete()
+        {
+            InitializeComponent();
+            displayProducts();
+        }
+
+        private void modify_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void displayProducts()
+        {
+            Business_Layer.Product product = new Business_Layer.Product();
+            int numberOfProduct = product.numberOfProduct();
+            Panel[] panels = new Panel[numberOfProduct];
+            int number = 0;
+            MySqlDataReader allProducts = product.showProduts();
+            while (allProducts.Read())
+            {
+                panels[number] = new Panel();
+                panels[number].Margin = new Padding(12, 0, 0, 20);
+                panels[number].Size = new Size(176, 245);
+                panels[number].BackColor = Color.WhiteSmoke;
+
+
+                Panel ProductImage = new Panel();
+                ProductImage.Size = new Size(150, 156);
+                ProductImage.Location = new Point(11, 19);
+                ProductImage.BackgroundImage = Image.FromFile("D:\\Muneer\\E-commerce\\E-commerce\\Presentation_Layer\\photo\\" + allProducts.GetValue(3).ToString());
+                ProductImage.BackgroundImageLayout = ImageLayout.Stretch;
+                ProductImage.Tag = number;
+               
+
+                Label productName = new Label();
+                productName.Text = allProducts.GetValue(1).ToString();
+                productName.Font = new Font("MV Boli", 9, FontStyle.Bold);
+                productName.Location = new Point(3, 180);
+                productName.Size = new Size(170, 25);
+                productName.ForeColor = Color.DimGray;
+                
+
+                Label productPrice = new Label();
+                productPrice.Text = allProducts.GetValue(2).ToString() + ".00$";
+                productPrice.ForeColor = Color.DimGray;
+                productPrice.Font = new Font("MV Boli", 9, FontStyle.Bold);
+                productPrice.Location = new Point(14, 212);
+                productPrice.Size = new Size(73, 18);
+
+
+                Button addButton = new Button();
+                addButton.Text = "Delete";
+                addButton.Font = new Font("Tahoma", 10, FontStyle.Bold);
+                addButton.BackColor = Color.BlueViolet;
+                addButton.ForeColor = Color.White;
+                addButton.Location = new Point(104, 207);
+                addButton.Size = new Size(64, 30);
+                addButton.FlatStyle = FlatStyle.Flat;
+                addButton.Cursor = Cursors.Hand;
+                addButton.Tag = allProducts.GetValue(0).ToString();
+                addButton.Click += addButton_Click;
+                 void addButton_Click(object sender, EventArgs e)
+                {
+                    Button clickedButton = (Button)sender;
+                    string id_product = (string)addButton.Tag;
+                    product.deleteProduct(id_product);
+                    MessageBox.Show(id_product);
+                    int number_product = (int)ProductImage.Tag;
+                    panels[number_product].Visible = false;
+                }
+
+                panels[number].Controls.Add(productName);
+                panels[number].Controls.Add(productPrice);
+                panels[number].Controls.Add(addButton);
+                panels[number].Controls.Add(ProductImage);
+                flowLayoutPanel1.Controls.Add(panels[number]);
+                number++;
+            }
+        }
+    }
+}
