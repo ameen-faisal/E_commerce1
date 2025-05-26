c779d136fcd82b2877cb5d7bcc99807ba652bdb0
eslammm-k <anbyl9687@gmail.com>
2025-05-26 23:11:03 +0300

    Products Page
    Products page to show the product information from database and can to add it to page cart

---
+++ E-commerce/Presentation_Layer/products.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace E_commerce.Presentation_Layer
+{
+    partial class products
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.flowLayoutPanel2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(786, 506);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AutoScroll = true;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 47);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(778, 458);
+            this.flowLayoutPanel2.TabIndex = 3;
+            this.flowLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel2_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Forte", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(292, 4);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(5);
+            this.label2.Size = new System.Drawing.Size(169, 40);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Our products";
+            // 
+            // products
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "products";
+            this.Size = new System.Drawing.Size(786, 506);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+    }
+}
---
+++ E-commerce/Presentation_Layer/products.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace E_commerce.Presentation_Layer
+{
+    public partial class products : UserControl
+    {
+        public products()
+        {
+            InitializeComponent();
+            displayProducts();
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel17_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        void displayProducts()
+        {
+            Business_Layer.Product product = new Business_Layer.Product();
+            MySqlDataReader allProducts = product.showProduts();
+            while (allProducts.Read())
+            {
+                Panel productContanir = new Panel();
+                productContanir.Margin = new Padding(12, 0, 0, 20);
+                productContanir.Size = new Size(176, 245);
+                productContanir.BackColor = Color.WhiteSmoke;
+                productContanir.Tag = allProducts.GetValue(0).ToString();
+
+
+                Panel ProductImage = new Panel();
+                ProductImage.Size = new Size(150, 156);
+                ProductImage.Location = new Point(11, 19);
+                ProductImage.BackgroundImage = Image.FromFile("D:\\Muneer\\E-commerce\\E-commerce\\Presentation_Layer\\photo\\"+ allProducts.GetValue(3).ToString());
+                //ProductImage.BackgroundImage = Image.FromFile(projrctPath + "\\Presentation_Layer\\photo\\" + allProducts.GetValue(3).ToString());
+                ProductImage.BackgroundImageLayout = ImageLayout.Stretch;
+
+                Label productName = new Label();
+                productName.Text = allProducts.GetValue(1).ToString();
+                productName.Font = new Font("MV Boli", 12, FontStyle.Bold);
+                productName.Location = new Point(3, 180);
+                productName.Size = new Size(170, 26);
+                productName.ForeColor = Color.DimGray;
+
+                Label productPrice = new Label();
+                productPrice.Text = allProducts.GetValue(2).ToString() + ".00$";
+                productPrice.ForeColor = Color.DimGray;
+                productPrice.Font = new Font("MV Boli", 9, FontStyle.Bold);
+                productPrice.Location = new Point(14, 212);
+                productPrice.Size = new Size(73, 18);
+
+
+                Button addButton = new Button();
+                addButton.Text = "Add";
+                addButton.Font = new Font("Tahoma", 10, FontStyle.Bold);
+                addButton.BackColor = Color.BlueViolet;
+                addButton.ForeColor = Color.White;
+                addButton.Location = new Point(104, 207);
+                addButton.Size = new Size(64, 30);
+                addButton.FlatStyle = FlatStyle.Flat;
+                addButton.Cursor = Cursors.Hand;
+                addButton.Tag = allProducts.GetValue(0).ToString();
+                addButton.Click += addButton_Click;
+                void addButton_Click(object sender, EventArgs e)
+                {
+                    Button clickedButton = (Button)sender;
+                    string id = (string)addButton.Tag;
+                    MessageBox.Show(id);
+                    product.insertToCart(id);
+                }
+
+                productContanir.Controls.Add(productName);
+                productContanir.Controls.Add(productPrice);
+                productContanir.Controls.Add(addButton);
+                productContanir.Controls.Add(ProductImage);
+                flowLayoutPanel2.Controls.Add(productContanir);
+            }
+        }
+
+        private void flowLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
