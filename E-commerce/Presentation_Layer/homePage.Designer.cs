1f9ac8d5ef1b34bb1f02c2b21ad9583261bcea68
mohamed alameri <mohamed.alameri02@gmail.com>
2025-05-26 23:28:43 +0300

    HomePage Form
    HomePage Forms as designer and code

---
+++ E-commerce/Presentation_Layer/homePage.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace E_commerce.Presentation_Layer
+{
+    partial class homePage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListBox listBox1;
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Forte", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(-3, 109);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(5);
+            this.label2.Size = new System.Drawing.Size(199, 54);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Everthing ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Forte", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(3, 163);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(5);
+            this.label1.Size = new System.Drawing.Size(287, 54);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "With One Click.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Forte", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.BlueViolet;
+            this.label3.Location = new System.Drawing.Point(169, 112);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(5);
+            this.label3.Size = new System.Drawing.Size(201, 54);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "You Want";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.BlueViolet;
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(39, 389);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(245, 47);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Shop Now";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::E_commerce.Properties.Resources.Black_Friday_amico__3_;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(348, 53);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(416, 385);
+            this.panel1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            listBox1.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            listBox1.ForeColor = System.Drawing.Color.DimGray;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 19;
+            listBox1.Items.AddRange(new object[] {
+            "Lorem ipsum dolor sit amet, consectetur ",
+            "adipisicing elit. Quaerat optio dolor aliquam ",
+            "quis numquam nesciunt quisquam,minus ",
+            "suscipit magni! Nisi aliquam saepe inventore ",
+            "voluptatemnostrum quisquam illo! Commodi,",
+            " amet aliquam."});
+            listBox1.Location = new System.Drawing.Point(23, 238);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new System.Drawing.Size(319, 133);
+            listBox1.TabIndex = 16;
+            // 
+            // homePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(listBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Name = "homePage";
+            this.Size = new System.Drawing.Size(786, 506);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+    }
+}
